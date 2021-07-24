f8a9b1215aaac57b4c667e9b024dba98d9589c95
Psylo2 <psylo20@hotmail.com>
2021-07-25 00:51:36 +0300

    change abc.model

--- models/abc/model.py
+++ models/abc/model.py
@@ -1,5 +1,5 @@
 from abc import ABCMeta
-from typing import TypeVar, Type, List
+from typing import TypeVar, Type, List, Dict
 from db.db import get_user, get_all_users
 
 T = TypeVar('T', bound="Model")
@@ -13,10 +13,13 @@ class Model(metaclass=ABCMeta):
     @classmethod
     def find_one_by(cls: Type[T], name_email: str) -> T:
         user = get_user(name_email)
-        d = {"_name": user[0], "_email": user[1], "_password": user[2]}
-        return cls(**d)
+        return cls(**cls.strip_tup(user))
 
     @classmethod
     def find_many_by(cls: Type[T]) -> List[T]:
         users = get_all_users()
-        return [cls(**{"_name": user[0], "_email": user[1], "_password": user[2]}) for user in users]
+        return [cls(**cls.strip_tup(user)) for user in users]
+
+    @classmethod
+    def strip_tup(cls: Type[T], tup: tuple) -> Dict:
+        return {"_name": tup[0], "_email": tup[1], "_password": tup[2]}
