88d82ee55ec207d111a0e661a5a6c2b4e364d5a8
Psylo2 <71320956+Psylo2@users.noreply.github.com>
2021-07-28 14:41:24 +0300

    Update model.py

--- models/abc/model.py
+++ models/abc/model.py
@@ -1,6 +1,7 @@
 from abc import ABCMeta
 from typing import TypeVar, Type, List, Dict
-from db.db import get_user, get_all_users, get_headers
+from db.db import (get_user, get_all_users, get_password,
+                   get_all_passwords, get_headers)
 
 T = TypeVar('T', bound="Model")
 
@@ -11,23 +12,21 @@ class Model(metaclass=ABCMeta):
         raise NotImplementedError
 
     @classmethod
-    def find_one_by(cls: Type[T], one: str, database: str) -> T:
-        if database == "user":
+    def find_one_by(cls: Type[T], one: str, header: str) -> T:
+        if header == "users":
             database = get_user(one)
-            headers = get_headers("users")
         else:
             database = get_password(one)
-            headers = get_headers("passwords")
+        headers = get_headers(header) #"passwords" or "users"
         return cls(**cls.strip_tup(headers, database))
 
     @classmethod
-    def find_many_by(cls: Type[T], database: str) -> List[T]:
-        if database == "user":
+    def find_many_by(cls: Type[T], header: str) -> List[T]:
+        if header == "users":
             database = get_all_users()
-            headers = get_headers("users")
         else:
             database = get_all_passwords()
-            headers = get_headers("passwords")
+        headers = get_headers(header) #"passwords" or "users"
         return [cls(**cls.strip_tup(headers, ele)) for ele in database]
 
     @classmethod
